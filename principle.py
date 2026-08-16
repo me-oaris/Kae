5a881c10e1d218cd319a65feb246a23de2645993
me-oaris <devilg5220@gmail.com>
2026-08-16 21:31:36 +0545

    Added json file input, for all the triggers and optimized the speed, and added Enter support

--- principle.py
+++ principle.py
@@ -1,34 +1,49 @@
 from pynput import keyboard
 from pynput.keyboard import Controller
-import time
+import json
 
 keyController = Controller()
 phrase = ""
-trigger = "!name"
-trigger_ans = "Lucian"
 
-def trigger_expand(trig):
-    if trig == trigger:
+def load_triggers():
+        with open("triggers.json", "r") as file:
+            return json.load(file)
+
+triggers = load_triggers()
+
+def click(key):
+    keyController.press(key)
+    keyController.release(key)
+
+def trigger_expand(trig,action):
+    if trig in triggers:
+        print(f"Found! Expanding {trig} to {triggers[trig]}")
+        keyController.press(keyboard.Key.shift)
         for _ in range(len(trig) + 1):
-            keyController.press(keyboard.Key.backspace)
-            keyController   .release(keyboard.Key.backspace)
-        time.sleep(0.1)
-        keyController.type(trigger_ans + " ")
+            click(keyboard.Key.left)
+        keyController.release(keyboard.Key.shift)
+        keyController.press(keyboard.Key.backspace)
+        keyController.release(keyboard.Key.backspace)
+        keyController.type(triggers[trig])
+        print(f"Expanded {trig} to {triggers[trig]}")
+        click(action)
+    else:
+        print(f"No trigger found for {trig}")
 
 def on_type(key):
     global phrase
     try:
-        if key == keyboard.Key.space:
+        if key == keyboard.Key.space or key == keyboard.Key.enter:
             if phrase.startswith("!"):
                 print("Trigger Detected")
-                trigger_expand(phrase)
+                print(f"Action: {key}")
+                trigger_expand(phrase,key)
+                phrase = ""
             else:
                 print(f"Typed phrase: {phrase}")
                 phrase = ""
         elif key == keyboard.Key.backspace:
             phrase = phrase[:-1]
-        elif key == keyboard.Key.enter:
-            phrase = ""
         else:
             phrase += key.char
     except AttributeError:
